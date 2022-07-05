78a5e170dd102a254a40d23d92c7a292b224de01
Maksymilian <ts3joker@gmail.com>
2022-07-05 18:01:10 +0200

    Initial Commit

---
+++ lesson9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lesson9
+//
+//  Created by Maksymilian Patoń on 05/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lesson9/lesson9App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson9App.swift
+//  lesson9
+//
+//  Created by Maksymilian Patoń on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct lesson9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
