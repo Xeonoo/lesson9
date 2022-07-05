9c648003fa4d2a17336bb042799c94bd3393a6f6
Maksymilian <ts3joker@gmail.com>
2022-07-05 18:34:58 +0200

    asd

--- lesson9/lesson9App.swift
+++ lesson9/lesson9App.swift
@@ -8,6 +8,31 @@
 import SwiftUI
 
 @main
+
+struct TaxCalculator {
+   
+   var tax = 0.15
+   
+   func totalWithTax(_ subtotal:Double) -> Double {
+       return subtotal * (1 + tax)
+   }
+}
+
+struct BillSplitter {
+   
+   func splitBy(subtotal:Double, numPeople:Double) -> Double {
+       
+       let taxCalc = TaxCalculator()
+       let totalWithTax = taxCalc.totalWithTax(subtotal)
+       
+       return totalWithTax/numPeople
+   }
+   
+}
+
+let split = BillSplitter()
+print(split.splitBy(subtotal: 120, numPeople: 5))
+
 struct lesson9App: App {
     var body: some Scene {
         WindowGroup {
