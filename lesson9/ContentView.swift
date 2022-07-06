d1c23d98f7158843fb138d64d9f6a7ba95d31957
Maksymilian <ts3joker@gmail.com>
2022-07-06 10:39:32 +0200

    button uses

--- lesson9/ContentView.swift
+++ lesson9/ContentView.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var Fbutton = 0
+    @State private var Sbutton = 0
+    
     var body: some View {
 //        Text("Hello, world!")
 //            .padding()
-        Button("Click") {
-            print("Yeeeey")
+        Button(String(Fbutton)) {
+            Fbutton += 2
         }
             Button {
-                print("no spk")
+                Sbutton *= 2
             } label: {
                 HStack {
                 Image(systemName: "play.fill")
-                Text("Pencil")
+                Text(String(Sbutton))
                 }
             }
     }
