55e1a1e2afd5e75f4390260b82386bedbb2c915f
Maksymilian <ts3joker@gmail.com>
2022-07-06 11:23:32 +0200

    50 to 0

--- lesson9/ContentView.swift
+++ lesson9/ContentView.swift
@@ -8,12 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
+       @State private var number = 0
+       @State private var shouldDecrease = false
+       
+       var body: some View {
+           
+           VStack {
+               Text(String(number))
+               .padding()
+               
+               Button("Click Me") {
+                   
+                   // Use the flag to determine which method to call
+                   if shouldDecrease == false {
+                       increase()
+                   }
+                   else {
+                       decrease()
+                   }
+                   // A little bit more advanced, but here's another way to write the above if statement:
+                   // shouldDecrease ? decrease() : increase()
+                   
+                   // See if we should toggle the flag
+                   if number > 50 {
+                       shouldDecrease = true
+                   }
+                   else if number < 0 {
+                       shouldDecrease = false
+                   }
+                   
+               }
+           }
+       }
+       
+       // Increase the number by a random amount
+       func increase() {
+       
+           let randAmount = Int.random(in: 1...10)
+           number += randAmount
+       }
+       
+       // Decrease the number by a random amount
+       func decrease() {
+           
+           let randAmount = Int.random(in: 1...10)
+           number -= randAmount
+       }
+   }
+    
+    /*
     @State private var Fbutton = 0
     @State private var Sbutton = 0
     
+ 
     var body: some View {
-//        Text("Hello, world!")
+        
+/       Text("Hello, world!")
 //            .padding()
         Button(String(Fbutton)) {
             Fbutton += 2
@@ -27,7 +78,7 @@ struct ContentView: View {
                 }
             }
     }
-}
+*/
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
